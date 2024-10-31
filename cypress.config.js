5969229d19ea9684d7941afe4441300bb7ae52aa
Bemnet14 <tewoldebemnet@gmail.com>
2024-10-31 23:19:04 +0100

    test 6

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-junit")({
+        // Configureer de JUnit reporter
+        mochaFile: "results/junit-[hash].xml", // Pas dit aan naar de gewenste outputlocatie
+      })(on);
     },
   },
 });
